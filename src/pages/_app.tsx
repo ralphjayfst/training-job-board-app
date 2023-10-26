1dda48e5709cdbbe44da5a7e798bd17f3ef2182f
Ralph Jay Pepito <ralph.jay@fullspeedtechnologies.com>
2023-10-26 12:17:32 +0800

    Remove not use code on datepickers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <LocalizationProvider dateAdapter={AdapterMoment}>
-      <Component {...pageProps} />
-    </LocalizationProvider>
-  )
+  return <Component {...pageProps} />
 }
