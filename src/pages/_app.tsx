40313d3aff99900ccc7ac084c31132c93e06a763
Ralph Jay Pepito <ralph.jay@fullspeedtechnologies.com>
2023-10-26 10:18:06 +0800

    Update current pages and interfaces

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,14 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Component {...pageProps} />
+    </LocalizationProvider>
+  )
 }
